4e97c7e8e328f9febfe763aba7eca35785dc86f0
Christopher Szeto <christopher.szeto@gmail.com>
2014-01-14 09:38:35 -0800

    Delete defaults.sh
    UCSC specific details that should be removed.

--- hgClassificationsViewer/hgClassificationsData/genome-cancer-su2c/defaults.sh
+++
@@ -1,3 +0,0 @@
-../loadClassificationResults -dropAll bioInt /hive/users/cszeto/grayBreast5azapredictions/results.ra
-../loadClassificationResults bioInt /hive/users/cszeto/grayBreast5azapredictionsLuminalOnly/results.ra
-../loadClassificationResults bioInt /hive/users/cszeto/grayBreast5azapredictionsBasalOnly/results.ra
